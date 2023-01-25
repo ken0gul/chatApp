707297b2ae00de761d1432ae612420455372b78b
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-25 15:43:10 -0500

    Initial commit

---
+++ src/main/java/com/ogulcan/chatapp/OgulcanChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ogulcan.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgulcanChatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgulcanChatProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogulcan/chatapp/OgulcanChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogulcan.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgulcanChatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
