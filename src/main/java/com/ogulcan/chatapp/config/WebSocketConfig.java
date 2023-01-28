20931d0b5ce4cad1d3b7229702033fb14b723a83
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-28 13:26:10 -0500

    Add StompEndPoint and create a SimpleMessageBroker

--- src/main/java/com/ogulcan/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/ogulcan/chatapp/config/WebSocketConfig.java
@@ -11,15 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
 	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/chat")
+		.setAllowedOriginPatterns("*")
+		.withSockJS();
+	}
+	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		// TODO Auto-generated method stub
-		WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+		registry.setApplicationDestinationPrefixes("/app")
+				.enableSimpleBroker("/topic");
 	}
 	
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		// TODO Auto-generated method stub
-		WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-	}
 
 }
