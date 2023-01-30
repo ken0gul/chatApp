509d8e3114e8081d4ab2ab1ca7b101b70b43934f
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-30 12:49:49 -0500

    Remove hashCode and equals cause it's useless here

--- src/main/java/com/ogulcan/chatapp/domain/User.java
+++ src/main/java/com/ogulcan/chatapp/domain/User.java
@@ -23,22 +23,7 @@ public class User {
 		return "User [username=" + username + "]";
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(username);
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(username, other.username);
-	}
 	
 	
 }
