d77ffab2db032b08933735c1473d631d6f8364cc
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-30 11:27:23 -0500

    Update UserController.java

--- src/main/java/com/ogulcan/chatapp/controller/UserController.java
+++ src/main/java/com/ogulcan/chatapp/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ogulcan.chatapp.controller;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,17 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ogulcan.tempStorage.UserTempStorage;
+import com.ogulcan.chatApp.tempStorage.UserTempStorage;
 
 @RestController
 @CrossOrigin
 public class UserController {
 
+//	private UserTempStorage storage = new UserTempStorage();
+	
 	@GetMapping("/register/{userName}")
 	public ResponseEntity<Void> register(@PathVariable String userName) {
-		System.out.println("handling register user request: " + userName);
+	
 		try {
 
+//			UserTempStorage.getInstance().setUser(userName);
 			UserTempStorage.getInstance().setUser(userName);
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
@@ -30,7 +34,8 @@ public class UserController {
 	
 	@GetMapping("/fetchAllUsers")
 	public Set<String> fetchAll() {
-		
-		return UserTempStorage.getInstance().getUsers();
+		return UserTempStorage.getInstance().getUsers().stream().map(u -> u.getName()).collect(Collectors.toSet());
+//		return UserTempStorage.getInstance().getUsers();
+//		return storage.getUsers();
 	}
 }
