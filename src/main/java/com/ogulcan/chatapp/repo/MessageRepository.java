9e72aa6866a6b3888d38a12d63184584c33ea6a5
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-02-02 12:27:08 -0500

    Update MessageRepository.java

--- src/main/java/com/ogulcan/chatapp/repo/MessageRepository.java
+++ src/main/java/com/ogulcan/chatapp/repo/MessageRepository.java
@@ -1,25 +1,33 @@
 package com.ogulcan.chatapp.repo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ogulcan.chatapp.domain.Message;
+
 @Repository
-public class MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+	List<Message> findBySenderIdOrReceiverId(Long senderId, Long receiverId);
 
 	
-	private List<String[]> myList = new ArrayList<>();
-	
-	public boolean save(String[] messages) {
-		myList.add(messages);
-	myList.forEach(i-> System.out.println(Arrays.toString(i)));
-		return true;
-	}
 	
 	
-	public List<String[]> getAllMessages(){
-		return myList;
-	}
+	
+	
+	
+//	private List<String[]> myList = new ArrayList<>();
+//	
+//	public boolean save(String[] messages) {
+//		myList.add(messages);
+//	myList.forEach(i-> System.out.println(Arrays.toString(i)));
+//		return true;
+//	}
+//	
+//	
+//	public List<String[]> getAllMessages(){
+//		return myList;
+//	}
 }
