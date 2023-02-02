0a4134487aa9fc7bb4dd20bda6c8a53c51fb7787
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-02-02 12:20:06 -0500

    Add @JsonBackReference annotation to avoid exceptions

--- src/main/java/com/ogulcan/chatapp/domain/User.java
+++ src/main/java/com/ogulcan/chatapp/domain/User.java
@@ -1,29 +1,111 @@
 package com.ogulcan.chatapp.domain;
 
-import java.util.Objects;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String username;
+	private String password;
+	private boolean isJoined = false;
+	
+	
+	@OneToMany(mappedBy = "sender",  fetch = FetchType.EAGER)
+	@JsonBackReference
+	private List<Message> sentMessages;
 
-	public User(String username) {
-		this.username = username;
+	
+	@OneToMany(mappedBy = "receiver",  fetch = FetchType.EAGER)
+	@JsonBackReference
+	private List<Message> receivedMessages;
+	
+	public User() {
+		
 	}
 	
-	public String getName() {
+	
+	
+	
+	public boolean isJoined() {
+		return isJoined;
+	}
+
+
+
+
+	public void setJoined(boolean isJoined) {
+		this.isJoined = isJoined;
+	}
+
+
+
+
+	public User(String username) {
+		this.username=username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
 		return username;
 	}
 
-	public void setName(String name) {
-		this.username = name;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	@Override
-	public String toString() {
-		return "User [username=" + username + "]";
+	
+
+//	@Override
+//	public String toString() {
+//		return "User [id=" + id + ", username=" + username + ", password=" + password + ", sentMessages=" + sentMessages
+//				+ ", receivedMessages=" + receivedMessages + "]";
+//	}
+
+	public List<Message> getSentMessages() {
+		return sentMessages;
 	}
 
+	public void setSentMessages(List<Message> sentMessages) {
+		this.sentMessages = sentMessages;
+	}
 
+	public List<Message> getReceivedMessages() {
+		return receivedMessages;
+	}
+
+	public void setReceivedMessages(List<Message> receivedMessages) {
+		this.receivedMessages = receivedMessages;
+	}
 	
-	
+
 }
